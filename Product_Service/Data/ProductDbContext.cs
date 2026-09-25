bd52731221fa7b83e3a7fcdada2a7c75e3e61068
gautam7379 <nchandan2004@gmail.com>
2026-09-25 12:19:55 +0530

    add: Models; add: Services; add: Data and Migrations;

---
+++ Product_Service/Data/ProductDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
